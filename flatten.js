70433151d81ef260c138f3a699903567a340ff45
amitkumarmountblue <amit.kumar.17@mountblue.tech>
2021-08-16 09:19:26 +0530

    Higher Order Array Function

--- flatten.js
+++ flatten.js
@@ -1,16 +1,13 @@
-const flatten = (items) => {  
-    const res = [];
-    function removeArr(items) {    
-      if (!Array.isArray(items)) {
-        res.push(items);
-      } else {
-        for ( let i=0; i<items.length;i++) {
-            removeArr(items[i]);
-          }
-      }
-      return res;
+const flatten = (items) => { 
+  let flatArray=[];
+  for(let i=0;i<items.length;i++){
+    if(Array.isArray(items[i])){
+      flatArray=flatArray.concat(flatten(items[i]));
+    }else{
+      flatArray.push(items[i]);
     }
-    return removeArr(items);
-  };
+  }
+  return flatArray;
+} 
 
   module.exports = flatten;
